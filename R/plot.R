f404edb0f769f7a24bf5ded20e09321c0a3d52df
Thibaut Jombart <thibautjombart@gmail.com>
2016-09-22 12:06:01 +0100

    new plot method using ggplot2

--- R/plot.R
+++ R/plot.R
@@ -1,4 +1,5 @@
 ##' @export
+##'
 ##' @importFrom graphics plot
 ##'
 ##' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
@@ -10,15 +11,31 @@
 ##'   inc
 ##'   plot(inc)
 ##' }
-plot.incidence <- function(x, interval = 1L, rolling = FALSE, ...,
-                           las = 1, xlab = "Days", ylab = "Cases") {
-  ylim <- c(0, max(x$cases))
-  plot(x$day, x$cases, type="s", ylim = ylim,
-       las = las, xlab = xlab, ylab = ylab)
+##'
+plot.incidence <- function(x, ..., xlab = "", ylab = NULL) {
+    df <- as.data.frame(x)
+
+    ## Use custom labels for usual time intervals
+    if(is.null(ylab)){
+        if(x$interval == 1) {
+            ylab <- "Daily incidence"
+        } else if(x$interval == 7) {
+            ylab <- "Weekly incidence"
+        } else if(x$interval == 14) {
+            ylab <- "Biweekly incidence"
+        } else {
+            ylab <- sprintf("Incidence by period of %d days", x$interval)
+        }
+    }
+
+    out <- ggplot2::ggplot(df, ggplot2::aes(x=dates, y=counts)) +
+        ggplot2::geom_bar(stat="identity", width=1, color="white") +
+            ggplot2::labs(x = xlab, y = ylab)
+    out
 }
 
 ##' @export
-plot.incidence_summary <- function(x, ..., ylab=NULL) {
+plot.incidence_summary <- function(x, ..., ylab = NULL) {
   if (is.null(ylab)) {
     ylab <- sprintf("Cases (%d day %s)", x$interval,
                     if (x$rolling) "rolling average" else "total")
