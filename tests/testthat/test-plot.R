297a9fdb0db98df5e8c370cbf6cc535517429197
Thibaut Jombart <thibautjombart@gmail.com>
2016-10-21 15:04:51 +0100

    adding complementary graphics tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -6,14 +6,19 @@ test_that("plot for incidence object", {
     dat <- sample(1:50, 200, replace = TRUE, prob = 1 + exp(1:50 * 0.1))
     sex <- sample(c("female", "male"), 200, replace = TRUE)
 
-    i <- incidence(dat, 3L)
+    i <- incidence(dat)
+    i.14 <- incidence(dat, 14L)
     i.sex <- incidence(dat, 7L, groups = sex)
     fit.i <- suppressWarnings(fit(i))
+    fit.i.2 <- suppressWarnings(fit(i, split = 30))
     fit.sex <- suppressWarnings(fit(i.sex))
 
+
     p.fit.i <- plot(fit.i)
+    p.fit.i.2 <- plot(i, fit = fit.i.2, color = "lightblue")
     p.fit.sex <- plot(fit.sex)
     p.i <- plot(i)
+    p.i.14 <- plot(i.14)
     p.i.2 <- plot(i, color = "blue", alpha=.2)
     p.i.3 <- plot(i, fit = fit.i, color = "red")
     p.sex <- plot(i.sex)
@@ -23,8 +28,10 @@ test_that("plot for incidence object", {
                    color = c(male = "salmon3",female = "gold2"))
 
     expect_equal_to_reference(p.fit.i, file = "rds/p.fit.i.rds")
+    expect_equal_to_reference(p.fit.i.2, file = "rds/p.fit.i.2.rds")
     expect_equal_to_reference(p.fit.sex, file = "rds/p.fit.sex.rds")
     expect_equal_to_reference(p.i, file = "rds/p.i.rds")
+    expect_equal_to_reference(p.i.14, file = "rds/p.i.14.rds")
     expect_equal_to_reference(p.i.2, file = "rds/p.i.2.rds")
     expect_equal_to_reference(p.i.3, file = "rds/p.i.3.rds")
     expect_equal_to_reference(p.sex, file = "rds/p.sex.rds")
