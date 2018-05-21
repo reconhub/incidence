2970ed86db45d0c5f41d55d23b4117376354c35b
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2018-05-21 14:34:53 +0100

    Error message if there is only 1 day with non-0 incidence
    Also wrote accompanying test.

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -21,6 +21,11 @@ test_that("fit", {
   expect_equal_to_reference(capture.output(fit.i), file="rds/print.fit.i.rds")
   expect_equal_to_reference(capture.output(fit.i.sex), file="rds/print.fit.sex.rds")
 
+  ## errors
+  x <- incidence(c(1, 0, 0, 0), interval = 7)
+  expect_error(fit(x), "Only 1 date with non-zero incidence. Cannot fit model to 1 data point.")
+
+
 })
 
 
