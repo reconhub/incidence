15d4cc7062175f0380f286537022334fc0469eb7
Thibaut Jombart <thibautjombart@gmail.com>
2018-05-31 11:07:41 +0100

    renamed palettes; plot_incidence_fit now calling add_incidence_fit

--- R/palettes.R
+++ R/palettes.R
@@ -1,43 +1,47 @@
-##' Color palettes used in incidence
-##'
-##' These functions are color palettes used in incidence.
-##'
-##' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
-##'
-##' @param n a number of colors
-##'
-##' @rdname palettes
-##' @aliases palettes pal1 pal1light pal1dark
-##'
-##' @export
-##' @importFrom grDevices colorRampPalette
-##'
-##' @examples
-##'
-##' plot(1:4, cex=8, pch=20, col = pal1(4), main="palette: pal1")
-##' plot(1:100, cex=8, pch=20, col = pal1(100), main="palette: pal1")
-##' plot(1:100, cex=8, pch=20, col = pal1light(100), main="palette: pal1light")
-##' plot(1:100, cex=8, pch=20, col = pal1dark(100), main="palette: pal1dark")
-##'
-pal1 <- function(n){
-    if(!is.numeric(n)) stop("n is not a number")
-    colors <- c("#aa3939", "#4a6a8a", "#d4aa6a","#499371")
-    if (n < 4) return(colors[1:n])
-    return(colorRampPalette(colors)(n))
+#' Color palettes used in incidence
+#'
+#' These functions are color palettes used in incidence.
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @param n a number of colors
+#'
+#' @rdname palettes
+#' @aliases palettes incidence_pal1 incidence_pal1_light incidence_pal1_dark
+#'
+#' @export
+#' @importFrom grDevices colorRampPalette
+#'
+#' @examples
+#'
+#' plot(1:4, cex=8, pch=20, col = incidence_pal1(4),
+#'      main = "palette: incidence_pal1")
+#' plot(1:100, cex=8, pch=20, col = incidence_pal1(100),
+#'      main ="palette: incidence_pal1")
+#' plot(1:100, cex=8, pch=20, col = incidence_pal1_light(100),
+#'      main="palette: incidence_pal1_light")
+#' plot(1:100, cex=8, pch=20, col = incidence_pal1_dark(100),
+#'      main="palette: incidence_pal1_dark")
+#'
+incidence_pal1 <- function(n){
+  if(!is.numeric(n)) stop("n is not a number")
+  colors <- c("#aa3939", "#4a6a8a", "#d4aa6a","#499371")
+  if (n < 4) return(colors[1:n])
+  return(colorRampPalette(colors)(n))
 }
 
 
 
 
 
-##' @export
-##' @rdname palettes
+#' @export
+#' @rdname palettes
 
-pal1light <- function(n){
-    if(!is.numeric(n)) stop("n is not a number")
-    colors <- c("#d46a6a", "#738ca6", "#ffddaa","#76b096")
-    if (n < 4) return(colors[1:n])
-    return(colorRampPalette(colors)(n))
+incidence_pal1_light <- function(n){
+  if(!is.numeric(n)) stop("n is not a number")
+  colors <- c("#d46a6a", "#738ca6", "#ffddaa","#76b096")
+  if (n < 4) return(colors[1:n])
+  return(colorRampPalette(colors)(n))
 }
 
 
@@ -45,12 +49,12 @@ pal1light <- function(n){
 
 
 
-##' @export
-##' @rdname palettes
+#' @export
+#' @rdname palettes
 
-pal1dark <- function(n){
-    if(!is.numeric(n)) stop("n is not a number")
-    colors <- c("#801515", "#2b4c6f", "#aa7d39","#277552")
-    if (n < 4) return(colors[1:n])
-    return(colorRampPalette(colors)(n))
+incidence_pal1_dark <- function(n){
+  if(!is.numeric(n)) stop("n is not a number")
+  colors <- c("#801515", "#2b4c6f", "#aa7d39","#277552")
+  if (n < 4) return(colors[1:n])
+  return(colorRampPalette(colors)(n))
 }
--- tests/testthat/test-palettes.R
+++ tests/testthat/test-palettes.R
@@ -1,30 +1,30 @@
 context("Color palettes")
 
-test_that("pal1", {
+test_that("incidence_pal1", {
   skip_on_cran()
 
-  expect_error(pal1(NULL), "n is not a number")
+  expect_error(incidence_pal1(NULL), "n is not a number")
   for (n in 1:30) {
-    expect_length(pal1(n), n)
+    expect_length(incidence_pal1(n), n)
   }
 })
 
-test_that("pal1light", {
+test_that("incidence_pal1_light", {
   skip_on_cran()
 
-  expect_error(pal1light(NULL), "n is not a number")
+  expect_error(incidence_pal1_light(NULL), "n is not a number")
   for (n in 1:30) {
-    expect_length(pal1light(n), n)
+    expect_length(incidence_pal1_light(n), n)
   }
 })
 
 
 
-test_that("pal1dark", {
+test_that("incidence_pal1_dark", {
   skip_on_cran()
 
-  expect_error(pal1dark(NULL), "n is not a number")
+  expect_error(incidence_pal1_dark(NULL), "n is not a number")
   for (n in 1:30) {
-    expect_length(pal1dark(n), n)
+    expect_length(incidence_pal1_dark(n), n)
   }
 })
