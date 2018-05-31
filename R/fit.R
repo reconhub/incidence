6ae645a1d1bd1e172d1bb1eabe244bae2f8f6517
Thibaut Jombart <thibautjombart@gmail.com>
2018-05-31 11:15:21 +0100

    added pipe example; passing check

--- R/fit.R
+++ R/fit.R
@@ -92,35 +92,40 @@
 #'
 #'  ## EXAMPLE WITH A SINGLE MODEL
 #'
-#'   ## compute weekly incidence
-#'   i.7 <- incidence(dat, interval=7)
-#'   plot(i.7)
-#'   plot(i.7[1:20])
+#'  ## compute weekly incidence
+#'  i.7 <- incidence(dat, interval=7)
+#'  plot(i.7)
+#'  plot(i.7[1:20])
 #'
-#'   ## fit a model on the first 20 weeks
-#'   f <- fit(i.7[1:20])
-#'   f
-#'   names(f)
-#'   head(f$pred)
+#'  ## fit a model on the first 20 weeks
+#'  f <- fit(i.7[1:20])
+#'  f
+#'  names(f)
+#'  head(f$pred)
 #'
-#'   ## plot model alone (not recommended)
-#'   plot(f)
+#'  ## plot model alone (not recommended)
+#'  plot(f)
 #'
 #'  ## plot data and model (recommended)
-#'  plot(i.7, fit=f)
-#'  plot(i.7[1:25], fit=f)
+#'  plot(i.7, fit = f)
+#'  plot(i.7[1:25], fit = f)
+#'
+#' ## piping versions
+#' if (require(magrittr)) {
+#'   plot(i.7) %>% add_incidence_fit(f)
 #'
 #'
-#' ## EXAMPLE WITH 2 PHASES
-#' ## specifying the peak manually
-#' f2 <- fit(i.7, split=as.Date("2014-10-15"))
-#' f2
-#' plot(i.7, fit=f2)
+#'   ## EXAMPLE WITH 2 PHASES
+#'   ## specifying the peak manually
+#'   f2 <- fit(i.7, split = as.Date("2014-10-15"))
+#'   f2
+#'   plot(i.7) %>% add_incidence_fit(f2)
 #'
-#' ## finding the best 'peak' date
-#' f3 <- fit_optim_split(i.7)
-#' f3
-#' plot(i.7, fit=f3$fit)
+#'   ## finding the best 'peak' date
+#'   f3 <- fit_optim_split(i.7)
+#'   f3
+#'   plot(i.7) %>% add_incidence_fit(f3$fit)
+#' }
 #' }
 #'
 
