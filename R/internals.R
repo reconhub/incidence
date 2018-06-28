d0e98fe8d269b6564474c6529f390297df51da84
Zhian N. Kamvar <zkamvar@gmail.com>
2018-06-28 17:12:29 +0100

    update interval checking

--- R/internals.R
+++ R/internals.R
@@ -74,7 +74,7 @@ check_dates <- function(x, error_on_NA = FALSE, ...) {
 
 ## Non-exported function, enforces that an interval is:
 ## - strictly positive
-## - integer (rounded)
+## - integer (rounded) OR compatibile with date
 ## - finite
 ## - of length 1
 check_interval <- function(x){
@@ -87,11 +87,15 @@ check_interval <- function(x){
       length(x)))
   }
   if (!is.finite(x)) {
-    # TODO: Add criteria for Date intervals
-    #
-    stop("Interval is not finite")
+    if (is.character(x)) {
+      x <- valid_interval_character(x)
+    } else {
+      stop("Interval is not finite")
+    }
   }
-  x <- as.integer(round(old <- x)) # TODO: remove this force
+  if (is.numeric(x)) {
+    x <- as.integer(round(old <- x))
+  }
   if (x < 1L) {
     stop(sprintf(
       "Interval must be at least 1 (input: %.3f; after rounding: %d)",
