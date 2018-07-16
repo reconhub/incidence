db1665c2b4ac3efeac5967953c4d2fbe3baf4aa0
Zhian N. Kamvar <zkamvar@gmail.com>
2018-07-16 15:38:19 +0100

    add get_dates

--- tests/testthat/test-accessors.R
+++ tests/testthat/test-accessors.R
@@ -15,10 +15,23 @@ test_that("get_interval works for integers", {
   expect_equal(get_interval(x.1, integer = FALSE), 1L)
 })
 
+test_that("get_dates works for integers", {
+  expect_equal(get_dates(x.1), x.1$dates)
+  expect_equal(get_dates(x.1, count_days = TRUE), seq_along(x.1$dates) - 1.0)
+  expect_equal(get_dates(x.1, "center"), x.1$dates + 0.5)
+  expect_equal(get_dates(x.1, "right"),  x.1$dates + 1)
+})
 
 test_that("get_interval works for integer weeks", {
   expect_equal(get_interval(x.7), 7L)
   expect_equal(get_interval(x.7, integer = FALSE), 7L)
+})
+
+test_that("get_dates works for integer weeks", {
+  expect_equal(get_dates(x.7), x.7$dates)
+  expect_equal(get_dates(x.7, count_days = TRUE), 7*(seq_along(x.7$dates) - 1.0))
+  expect_equal(get_dates(x.7, "center"), x.7$dates + 3.5)
+  expect_equal(get_dates(x.7, "right"),  x.7$dates + 7)
 })
 
 
@@ -27,16 +40,36 @@ test_that("get_interval works for character days", {
   expect_equal(get_interval(x.day, integer = FALSE), "day")
 })
 
+test_that("get_dates works for character days", {
+  expect_equal(get_dates(x.day), x.day$dates)
+  expect_equal(get_dates(x.day, count_days = TRUE), (seq_along(x.day$dates) - 1.0))
+  expect_equal(get_dates(x.day, "center"), x.day$dates + 0.5)
+  expect_equal(get_dates(x.day, "right"),  x.day$dates + 1.0)
+})
 
-test_that("get_interval works for x", {
+test_that("get_interval works for character weeks", {
   expect_equal(get_interval(x.wee), 7L)
   expect_equal(get_interval(x.wee, integer = FALSE), "week")
 })
 
+test_that("get_dates works for character weeks", {
+  expect_equal(get_dates(x.wee), x.wee$dates)
+  expect_equal(get_dates(x.wee, count_days = TRUE), 7*(seq_along(x.wee$dates) - 1.0))
+  expect_equal(get_dates(x.wee, "center"), x.wee$dates + 3.5)
+  expect_equal(get_dates(x.wee, "right"),  x.wee$dates + 7.0)
+})
 
-test_that("get_interval works for x", {
+
+test_that("get_interval works for character months", {
   expect_equal(get_interval(x.mon), c(31, 28, 31))
   expect_equal(get_interval(x.mon, integer = FALSE), "month")
+})
+
+test_that("get_dates works for character months", {
+  expect_equal(get_dates(x.mon), x.mon$dates)
+  expect_equal(get_dates(x.mon, count_days = TRUE), c(0, 31, 59))
+  expect_equal(get_dates(x.mon, "center"), x.mon$dates + c(31, 28, 31)/2)
+  expect_equal(get_dates(x.mon, "right"),  x.mon$dates + c(31, 28, 31))
 })
 
 test_that("errors happen", {
