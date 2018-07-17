55918f4cf265050a8752bafa764087118f7a796d
Zhian N. Kamvar <zkamvar@gmail.com>
2018-07-17 14:07:59 +0100

    allow flexibility with months

--- R/internals.R
+++ R/internals.R
@@ -41,9 +41,16 @@ make_incidence <- function(dates, interval = 1L, groups = NULL,
   groups   <- check_groups(groups, dates, na_as_group)
 
   ## Check the interval and arrange the breaks
-  first_date <- check_boundaries(dates, first_date, "first")
-  last_date  <- check_boundaries(dates, last_date, "last")
-  breaks     <- make_breaks_easier(dates, interval, first_date, last_date, dots)
+  null_first_date <- is.null(first_date)
+  first_date      <- check_boundaries(dates, first_date, "first")
+  last_date       <- check_boundaries(dates, last_date, "last")
+  breaks          <- make_breaks_easier(dates,
+                                        the_interval    = interval,
+                                        first_date      = first_date,
+                                        last_date       = last_date,
+                                        dots            = dots,
+                                        null_first_date = null_first_date
+                                        )
 
   ## Trim the dates
   dates <- trim_dates(dates, first_date, last_date)
@@ -103,6 +110,8 @@ trim_dates <- function(dates, first_date = NULL, last_date = NULL) {
 #' @param last_date an integer, numeric, or Date
 #' @param first_date an integer, numeric, or Date
 #' @param dots a named list of options
+#' @param null_first_date a logical specifying whether or not the first_date
+#'   argument was NULL in the original call.
 #'
 #' @author Zhian Kamvar
 #' @return a vector of integers or Dates
@@ -112,29 +121,69 @@ trim_dates <- function(dates, first_date = NULL, last_date = NULL) {
 #' set.seed(999)
 #' d <- sample(10, replace = TRUE)
 #' make_breaks_easier(d, 2L)
-make_breaks_easier <- function(dates, the_interval, first_date = NULL, last_date = NULL, dots = 1L) {
+make_breaks_easier <- function(dates, the_interval, first_date = NULL,
+                               last_date = NULL, dots = 1L,
+                               null_first_date = TRUE) {
 
-  the_interval  <- valid_interval_character(the_interval)
-  date_interval <- is.character(the_interval) && is_date_interval(the_interval)
+  the_interval    <- valid_interval_character(the_interval)
+  date_interval   <- is.character(the_interval) && is_date_interval(the_interval)
+  uneven_interval <- date_interval && the_interval %in% c("month", "quarter", "year")
 
-  if ("iso" %in% names(dots) || date_interval) {
-    is_a_week <- check_week(the_interval)
-    if (is_a_week && (identical(dots$iso, TRUE) || date_interval)) {
-      first_isoweek <- ISOweek::date2ISOweek(first_date)
-      substr(first_isoweek, 10, 10) <- "1"
-      first_date <- ISOweek::ISOweek2date(first_isoweek)
+  # getting information about the date
+  fd        <- as.character(first_date)
+  the_day   <- as.integer(substr(fd, 9, 10))
+  the_month <- as.integer(substr(fd, 6, 7))
+
+  if ("iso" %in% names(dots)) {
+    if (isTRUE(dots$iso) && null_first_date) {
+      is_a_week <- !uneven_interval && check_week(the_interval)
+      if (is_a_week) {
+        # This returns something like 2018-W29-2, where the last digit indicates
+        # the day of the week
+        first_isoweek <- ISOweek::date2ISOweek(first_date)
+        # Here, we force it to be the start of the week
+        substr(first_isoweek, 10, 10) <- "1"
+        # and convert it back
+        first_date <- ISOweek::ISOweek2date(first_isoweek)
+      }
+      if (uneven_interval) {
+        # Replace the day with the first day of the month
+        substr(fd, 9, 10) <- "01"
+        if (the_interval == "quarter") {
+          # Replace the month with the first month of the quarter
+          m <- (as.integer(substr(fd, 6, 7)) - 1L) %/% 3L
+          substr(fd, 6, 7) <- sprintf("%02d", (m * 3) + 1L)
+        }
+        if (the_interval == "year") {
+          # Replace the month with the first month of the year
+          substr(fd, 6, 7) <- "01"
+        }
+        # re-cast the date
+        first_date <- as.Date(fd)
+      }
+    } else {
+      if (uneven_interval && the_interval != "year" && the_day > 28) {
+        # The first date represents a day that doesn't occur in all months
+        msg <- paste("The first_date (%s) represents a day that does not",
+                     "occur in all months. Because of this, bins may not",
+                     "conform to monthly boundaries. To prevent this",
+                     "behavior, plese specify a different first_date that",
+                     "represents a day within [1, 28]."
+                     )
+        msg <- paste(strwrap(msg), collapse = "\n")
+        warning(sprintf(msg, fd), call. = FALSE)
+      }
+      if (the_interval == "year" && the_month == 2 && the_day == 29) {
+        # The first date occurs on a leap day.
+        msg <- paste("The first_date (%s) represents a day that does not",
+                     "occur in all years. Because of this, bins may not",
+                     "fall on the same day. To prevent this behavior, please",
+                     "specify a first_date that represents a different day."
+                     )
+        msg <- paste(strwrap(msg), collapse = "\n")
+        warning(sprintf(msg, fd), call. = FALSE)
+      }
     }
-  }
-  if (date_interval && the_interval %in% c("month", "quarter", "year")) {
-    fd <- as.character(first_date)
-    # Replace the day with the first day of the month
-    substr(fd, 9, 10) <- "01"
-    if (the_interval %in% "year") {
-      # Replace the month with the first month of the year
-      substr(fd, 6, 7) <- "01"
-    }
-    # re-cast the date
-    first_date <- as.Date(fd)
   }
   seq(first_date, last_date, by = the_interval)
 }
