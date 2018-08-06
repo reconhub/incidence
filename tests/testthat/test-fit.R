0ae8ade7f0994a41e05df48909da9fb908ae14f8
Zhian N. Kamvar <zkamvar@gmail.com>
2018-08-06 17:23:44 +0100

    update tests

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -32,13 +32,21 @@ test_that("fit_optim_split", {
            sample(51:100, 200, replace = TRUE, prob = rev(1 + exp(1:50 * 0.1))))
   sex <- sample(c("female", "male"), 400, replace = TRUE)
 
-  i <- incidence(dat, 5L)
+  i     <- incidence(dat, 5L)
   i.sex <- incidence(dat, 5L, groups = sex)
-
-  expect_equal_to_reference(fit_optim_split(i, plot = FALSE),
+  i.fit     <- fit_optim_split(i, plot = FALSE)
+  i.fit.sex <- fit_optim_split(i.sex, plot = FALSE)
+  expect_equal_to_reference(i.fit,
                             file = "rds/o.fit.i.rds")
-  expect_equal_to_reference(fit_optim_split(i.sex, plot = FALSE),
+  expect_equal_to_reference(i.fit.sex,
                             file = "rds/o.fit.i.sex.rds")
+
+  expect_is(i.fit$df, "data.frame")
+  expect_is(i.fit$fit, "incidence_fit_list")
+
+  expect_is(i.fit.sex$df, "data.frame")
+  expect_is(i.fit.sex$fit, "incidence_fit_list")
+
 
   ## errors
   expect_error(fit_optim_split(i, window = -1),
