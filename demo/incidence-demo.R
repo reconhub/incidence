bbc94baa64d1c31b780adc1efb67c847673aefeb
Zhian N. Kamvar <zkamvar@gmail.com>
2018-11-29 10:43:53 +0000

    update incidence demo

--- demo/incidence-demo.R
+++ demo/incidence-demo.R
@@ -1,7 +1,7 @@
 #' Example 1 ------------------------------------------------------------------
-#' 
+#'
 #' **computing and manipulating stratified weekly incidence**
-#' 
+#'
 #' 1) import data
 #'
 library('outbreaks')
@@ -16,29 +16,38 @@ library('incidence')
 library('ggplot2')
 
 # compute weekly stratified incidence
-i.7.group <- incidence(dat1$date_of_onset, 
-		       interval = 7L,
-                       groups = dat1$hospital)
+i.7.group <- incidence(dat1$date_of_onset,
+                       interval = 7L,
+                       groups   = dat1$hospital)
 # print incidence object
 i.7.group
 
 # plot incidence object
-my_theme <- theme_bw(base_size = 12) + 
+my_theme <- theme_bw(base_size = 12) +
   theme(legend.position = c(.8, .7)) +
-  theme(axis.text = element_text(color = "black")) 
+  theme(panel.grid.minor = element_blank()) +
+  theme(axis.text = element_text(color = "black"))
 plot(i.7.group, border = "white") + my_theme
 
 #' 3) Manipulate incidence object
 #'
+# plot the first 8 weeks
+plot(i.7.group[1:8, ], show_cases = TRUE) +
+  theme_bw(base_size = 12) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, color = "black")) +
+  theme(panel.grid.minor = element_blank())
+
 # exclude NA group by disabling treating NA as a separate group
-i.7.group0 <- incidence(dat1$date_of_onset, interval = 7L,
-                       groups = dat1$hospital, na_as_group = FALSE)
+i.7.group0 <- incidence(dat1$date_of_onset,
+                        interval    = 7L,
+                        groups      = dat1$hospital,
+                        na_as_group = FALSE)
 
 # exclude NA using [ operator
-i.7.group1 <- subset(i.7.group, groups = -3)
+i.7.group1 <- subset(i.7.group, groups = -ncol(i.7.group))
 
 # exclude NA group using [ operator
-i.7.group2 <- i.7.group[, -3]
+i.7.group2 <- i.7.group[, -ncol(i.7.group)]
 
 # check the resulting incidence objects are identical
 identical(i.7.group0$counts, i.7.group1$counts)
@@ -67,24 +76,24 @@ names(dat2)[2:3] <- c("Girardot", "San Andres")
 # replace NA with 0
 dat2[is.na(dat2)] <- 0
 
-# convert pre-computed incidence in data.frame into incidence object 
+# convert pre-computed incidence in data.frame into incidence object
 # grouped by locations
 i.group <- as.incidence(x = dat2[, 2:3], dates = dat2$date)
 
 # pool incidence across two locations
 i.pooled <- pool(i.group)
 cowplot::plot_grid(
-	plot(i.group, border = "white") + my_theme,
+	plot(i.group, border = "white")  + my_theme,
 	plot(i.pooled, border = "white") + my_theme,
-	ncol = 1,
-	labels = c("(A)", "(B)"),
+	ncol       = 1,
+	labels     = c("(A)", "(B)"),
 	label_size = 16,
-	label_x = 0.06,
-	label_y = 0.94
+	label_x    = 0.06,
+	label_y    = 0.94
 )
 
 #' 2) Fit log-linear regression model
-#' 
+#'
 #+ incidence-fit, fig.width=9, fig.height=4
 library('magrittr')
 
@@ -94,3 +103,4 @@ fos$fit
 plot(i.pooled, border = "white") %>%
   add_incidence_fit(fos$fit) +
   my_theme
+
