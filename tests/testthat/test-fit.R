1a07c205a1c6af392476b5a35108ad93b9d745d6
jrcpulliam <juliet@ici3d.org>
2018-12-12 17:09:21 +0100

    minor update to test for #91 (doesn't affect anything, just modifying to be as intended)

--- tests/testthat/test-fit.R
+++ tests/testthat/test-fit.R
@@ -65,7 +65,7 @@ test_that("internals for fitting", {
 test_that("fitting results are the same for incidence fits on Dates and POSIXct", {
   days <- 1:14
   dat_cases <- round(exp(.2*(days)))
-  dat_dates_Date <- rep(as.Date(Sys.Date()+days), days)
+  dat_dates_Date <- rep(as.Date(Sys.Date()+days), dat_cases)
   dat_dates_POSIXct <- as.POSIXct(dat_dates_Date)
 
   iD <- incidence(dat_dates_Date)
